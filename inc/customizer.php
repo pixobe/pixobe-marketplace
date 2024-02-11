bf3c42f307b1d36372b403203d9ba4dd91da04e9
pixobe <email@pixobe.com>
2024-02-11 11:55:22 +0800

    init

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * pixobe-marketplace Theme Customizer
+ *
+ * @package pixobe-marketplace
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function pixobe_marketplace_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'pixobe_marketplace_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'pixobe_marketplace_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'pixobe_marketplace_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function pixobe_marketplace_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function pixobe_marketplace_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function pixobe_marketplace_customize_preview_js() {
+	wp_enqueue_script( 'pixobe-marketplace-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'pixobe_marketplace_customize_preview_js' );
